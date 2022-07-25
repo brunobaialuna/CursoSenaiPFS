c7c9a151a36a48bfc0badf9c2c36adce12c80078
Bruno Baialuna <brunobaialuna@hotmail.com>
2022-07-25 08:18:51 -0300

    Adicionado o modulo de interação com o usuário

--- UC8_Logica_de_Programacao/Encontro_Virtual_1_Cadastro_de_Pecas/cadastro_de_pecas.js
+++ UC8_Logica_de_Programacao/Encontro_Virtual_1_Cadastro_de_Pecas/cadastro_de_pecas.js
@@ -1,14 +1,20 @@
 let listadepecas = [];
 
-while (listadepecas.length < 10) {
+do {
     let pesodapeca = prompt("Digite o peso da peça: ");
     if (pesodapeca > 100) {
         let nomedapeca = prompt("Digite o nome da peça: ");
         if (nomedapeca.length >= 3) {
             listadepecas.push(nomedapeca);
             console.log(listadepecas);
+            let continuar = prompt("Deseja cadastrar outra peça? S / N")
+            if (continuar == "S" || continuar == 's') {
+                continue;
+            } else {
+                break;
+            }
         } else {
-            console.log("O nome da peça é inferior a 3 caracteres.");
+            alert("O nome da peça é inferior a 3 caracteres.");
             respostaNome = prompt("Deseja cadastrar uma nova peça? S / N");
             if (respostaNome == "S" || respostaNome == "s") {
                 continue
@@ -17,7 +23,7 @@ while (listadepecas.length < 10) {
             }
         }
     } else {
-        console.log("O peso da peça é inválido.");
+        alert("O peso da peça é inválido.");
         respostaPeso = prompt("Deseja cadastrar uma nova peça? S / N");
         if (respostaPeso == "S" || respostaPeso == "s") {
             continue
@@ -26,6 +32,9 @@ while (listadepecas.length < 10) {
         }
     }
 }
+
+while (listadepecas.length < 10)
+
 if (listadepecas.length == 10) {
     console.log("A caixa atingiu o número máximo de peças.")
 }
